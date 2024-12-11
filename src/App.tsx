ab9a0125ee80e78ddcda3f2c3febb470a2fd87e6
martincham <martinchamberlin12@gmail.com>
2024-12-10 22:02:17 -0500

    ouch

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>ouch.house</h1>
-    </>
+    <div className="page">
+      <div className="black">
+        <h1>&nbsp;ouch.house &nbsp;</h1>
+      </div>
+    </div>
   );
 }
 
